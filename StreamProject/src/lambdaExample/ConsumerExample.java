5d42f4e9ba710d2b9c0c6632cad1269dcbc0b717
admin <admin@YD03-20>
2020-06-18 17:49:25 +0900

    2020-06-18

--- StreamProject/src/lambdaExample/ConsumerExample.java
+++ StreamProject/src/lambdaExample/ConsumerExample.java
@@ -1,6 +1,9 @@
 package lambdaExample;
 
+import java.util.function.BiConsumer;
 import java.util.function.Consumer;
+import java.util.function.DoubleConsumer;
+import java.util.function.ObjIntConsumer;
 
 public class ConsumerExample {
 	public static void main(String[] args) {
@@ -12,7 +15,36 @@ public class ConsumerExample {
 			}
 			
 		};
-		
 		consumer.accept("java");
+		
+		BiConsumer<String, String> bigConsumer = new BiConsumer<String, String>() {
+
+			@Override
+			public void accept(String t, String u) {
+				System.out.println(t + u);
+			}
+			
+		};
+		bigConsumer.accept("java", "8");
+		
+		DoubleConsumer doubleConsumer = new DoubleConsumer() {
+
+			@Override
+			public void accept(double value) {
+				System.out.println("Java"+value);
+			}
+			
+		};
+		doubleConsumer.accept(8.0);
+		
+		ObjIntConsumer<String> objIntconsumer = new ObjIntConsumer<String>() {
+
+			@Override
+			public void accept(String t, int value) {
+				System.out.println(t+value);
+			}
+			
+		};
+		objIntconsumer.accept("java", 8);
 	}
 }
